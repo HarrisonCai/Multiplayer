05f83f474b8ac7784c269ed1a9df3619fe61762d
CornFlakes <122186514+HarrisonCai@users.noreply.github.com>
2024-02-02 17:58:58 -0500

    movement fix

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class MovementController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] [Range(0.3f,20)]private float multiplier;
-    Vector2 speed;
+    [SerializeField] [Range(0.3f,100)]private float multiplier;
+    private Vector2 speed;
+    private float sin, cos;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +17,27 @@ public class MovementController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = Vector2.zero;
+
+        
         float v = Input.GetAxis("Vertical");
         float h = Input.GetAxis("Horizontal");
-        speed = new Vector2(h , v ).normalized;
 
-        rb.MovePosition(rb.position + speed * multiplier * Time.fixedDeltaTime);
+        if (v != 0 || h != 0)
+        {
+            
+            sin = Mathf.Abs(v / (Mathf.Sqrt(Mathf.Pow(v, 2) + Mathf.Pow(h, 2))));
+            cos = Mathf.Abs(h / (Mathf.Sqrt(Mathf.Pow(v, 2) + Mathf.Pow(h, 2))));
+            Debug.Log(v + " : " + h + " : " + sin + " : " + cos);
+            speed = new Vector2(h * cos, v * sin);
+        }
+        else
+        {
+            speed = Vector2.zero; 
+        }
+        Debug.Log(speed);
+
+        rb.velocity = speed * multiplier;
+        
+        
     }
 }
