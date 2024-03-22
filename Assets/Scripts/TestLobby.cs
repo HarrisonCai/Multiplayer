6b3d35906ccc7bdd872d40e50d8e5c891483dcc5
Daniel Ye <107735388+DanieLLOnGit@users.noreply.github.com>
2024-03-21 22:47:13 -0400

    lobby attemp #1

---
+++ Assets/Scripts/TestLobby.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
+using UnityEngine;
+
+public class TestLobby : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private async void Start()
+    {
+        await UnityServices.InitializeAsync();
+
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+            Debug.Log("Signed in " + AuthenticationService.Instance.PlayerId);
+        };
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    private async void CreateLobby()
+    {
+        try
+        {
+            string lobbyName = "MyLobby";
+            int maxPlayers = 4;
+            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
+
+            Debug.Log("Created Lobby! " + lobby.Name + " " + lobby.MaxPlayers);
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+}
