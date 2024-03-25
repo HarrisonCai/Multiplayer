1aad4e269e54f9d56e565245e65abc7513ee7ae2
CornFlakes <122186514+HarrisonCai@users.noreply.github.com>
2024-03-25 00:15:21 -0400

    oop

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
-using Unity.UI;
+using UnityEngine.UI;
 using TMPro;
 public class Health : NetworkBehaviour
 {
